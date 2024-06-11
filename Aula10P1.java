8f4b13006067915c35d6ffe7b9de03ac8d6f811e
Natan Henrique <natan.nh20@gmail.com>
2024-06-10 21:58:40 -0300

    Melhoria

--- Aula10P1.java
+++ Aula10P1.java
@@ -3,24 +3,20 @@ import java.util.Scanner;
 public class Aula10P1 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        boolean trava = true;
         int soma = 0;
+        int num = scn.nextInt();
         /*Faça um algoritmo que pede para o usuário digitar números inteiros
         positivos. O algoritmo deve somar esses números e parar quando o
         usuário digitar um número negativo.
         */
         
-            while (trava == true) {
+            while (num >= 0) {
                 System.out.print("Digite um numero inteiro diferente de 0: ");
-                int num = scn.nextInt();
-                if (num > 0){
+
+                if (num >= 0){
                     soma += num;
                     System.out.println("A soma atual é igual a " + soma);
-                } else {
-                    trava = false;
-                }
-                
-                
+                } 
             }
 
         scn.close();
