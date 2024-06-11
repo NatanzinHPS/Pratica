150ec9ae8f72f7cf72c56313a308380d5cc5d5e6
Natan Henrique <natan.nh20@gmail.com>
2024-06-10 21:34:36 -0300

    Alteracao

--- Aula10P1.java
+++ Aula10P1.java
@@ -10,15 +10,12 @@ public class Aula10P1 {
         usuário digitar um número negativo.
         */
         
-
             while (trava == true) {
                 System.out.print("Digite um numero inteiro diferente de 0: ");
                 int num = scn.nextInt();
                 if (num > 0){
                     soma += num;
                     System.out.println("A soma atual é igual a " + soma);
-                } else if (num == 0){
-                    System.out.println("?");
                 } else {
                     trava = false;
                 }
